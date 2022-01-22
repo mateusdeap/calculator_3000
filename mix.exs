76d3583398a9c76f95a53ebc0596ba13c208125a
Mateus Pereira <mateus@ombulabs.com>
2022-01-22 14:24:48 -0300

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Calculator3000.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :calculator_3000,
+      version: "0.1.0",
+      elixir: "~> 1.13-dev",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
